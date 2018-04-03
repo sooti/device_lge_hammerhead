91f71f0f419db57c59e83d44cf30bf91c74c8125
Krizthian <cristianortizmorales2013@gmail.com>
2018-04-03 08:03:35 -0500

    hammerhead: Update

--- lineage.mk
+++ lineage.mk
@@ -38,4 +38,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
     PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys"
 
-BUILD_FINGERPRINT := google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys 
+BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys 
