d116cf67f81deac0b790842599d167ed32dca2b0
Krizthian <cristianortizmorales2013@gmail.com>
2018-04-12 06:36:25 -0500

    hammerhead: Build LiveDisplay HAL

--- device.mk
+++ device.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
     libqdutils \
     libqdMetaData
 
+#LiveDisplay HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Dumpstate HAL
 PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.0-service.hammerhead
