ea23e3f111db6dd9f76647b66b878597b7a5cacd
sooti <sooti85@gmail.com>
2018-05-08 07:02:55 +0000

    hammerhead: switch to lineage power hal 1.0

--- device.mk
+++ device.mk
@@ -204,8 +204,7 @@ PRODUCT_PACKAGES += \
     keystore.msm8974
 
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8974
+    android.hardware.power@1.0-service-qti
 
 
 # Thermal HAL
